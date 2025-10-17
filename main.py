b38378dfb6dfcb2cb43afc36688fa551468388fb
JoaoAbreu-sccp <jvsabru@gmail.com>
2025-10-17 20:07:07 -0300

    lista alunos + arquivo lista

--- main.py
+++ main.py
@@ -54,14 +54,25 @@ Bem vindo, {linhas[posicao_senha+1]}
         opc = int(input("opc = "))
         if opc == 1:
             screen.clear()
-            print('''
-Lista de Alunos:
-
-1- fulano
-2- beltrano
-
-[1] Voltar''')
-            opc = int(input("opc = "))
+            listaalunos = open("listaalunos.txt")
+            linhasalunos = listaalunos.read().splitlines()
+            listaalunos.close()
+            print("Lista de Alunos:")
+            print(f"{'#':3}{'Nome':21}{'Instituição':21}  {'P. Embarque':21}  {'P. Desembarque':21}  {'Conf. Ida':21}  {'Conf. Volta':21}")
+            index = 0
+            for c, info in enumerate(linhasalunos):
+                if c % 7 == 0 or c == 0:
+                    if info != "":
+                        index += 1
+                        print(f"{index}. {info:18}", end=' | ')
+                    else:
+                        print()
+                else:
+                    if info != "":
+                        print(f"{info:20}", end=' | ')
+                    else:
+                        print()
+            opc = int(input("\nDigite [1] para voltar:  "))
             sleep(2)
             screen.clear()
         elif opc == 2:
