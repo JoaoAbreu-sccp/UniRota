d1c7c11ed0a539a4133167e976ac646666c96922
Guilherme-Coelho10 <guilhermepereiracoelho100@gmail.com>
2025-11-12 00:10:20 -0300

    Atualização de rota + rota.txt + mod menores

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functions import GeralDef, screen
 from time import sleep
 
+posicao_tipo = None  
 
 while True:
     logins = open("logins.txt")
@@ -41,11 +42,125 @@ while True:
 
     if opc == 3:
         break
-if linhas[posicao_tipo] == "administrador":
-    print("Adm entrou")
 
-if linhas[posicao_tipo] == "aluno":
-    print("Aluno entrou")
+if posicao_tipo is not None:
 
-if linhas[posicao_tipo] == "motorista":
-    print("motorista entrou")
+    if linhas[posicao_tipo] == "administrador":
+        print("Adm entrou")
+
+    if linhas[posicao_tipo] == "aluno":
+        print("Aluno entrou")
+
+    if linhas[posicao_tipo] == "motorista":
+        print("motorista entrou")
+
+        while True:
+            screen.clear()
+            print(f"Bem vindo, {linhas[posicao_senha+1]}")
+            screen.menuMOT()
+            opc = int(input("opc = "))
+            if opc == 1:
+                screen.clear()
+                listaalunos = open("listaalunos.txt")
+                linhasalunos = listaalunos.read().splitlines()
+                listaalunos.close()
+                print("Lista de Alunos:")
+                print(f"{'#':3}{'Nome':21}{'Instituição':21}  {'P. Embarque':21}  {'P. Desembarque':21}  {'Conf. Ida':21}  {'Conf. Volta':21}")
+                index = 0
+                for c, info in enumerate(linhasalunos):
+                    if c % 7 == 0 or c == 0:
+                        if info != "":
+                            index += 1
+                            print(f"{index}. {info:18}", end=' | ')
+                        else:
+                            print()
+                    else:
+                        if info != "":
+                            print(f"{info:20}", end=' | ')
+                        else:
+                                print()
+                opc = int(input("\nDigite [1] para voltar:  "))
+                sleep(0.5)
+                screen.clear()
+
+            elif opc == 2:
+                screen.clear()
+                print("Rota")
+                print()
+                with open("rota.txt") as arquivo:
+                    arq = arquivo.read().splitlines()
+
+                print(f"{'#':<3} | {'Check':<11} | {'Ponto de Parada':<40} | {'Endereço':<50}")
+                print("-" * 105)
+
+                for i in range(0, len(arq), 3):
+                    ponto = arq[i + 1].strip()
+                    end = arq[i + 2]
+                    
+                    if i + 2 < len(arq):
+                        check = arq[i].strip()
+                    else:
+                        check = ""
+
+                    if check == "":
+                        print(f"{i // 3 + 1:<3} | {'/':<11} | {ponto:<40} | {end:<50}")
+                        newcheck = input("Check [True|False]: ").strip()
+                        print()
+
+                        arq[i] = newcheck
+
+                        with open("rota.txt", "w") as rota:
+                            rota.write("\n".join(arq))
+                    else:
+                        print()
+                        print(f"{i // 3 + 1:<3} | {check:<11} | {ponto:<40} | {end:<50}")
+
+                        if check == "False":
+                            mod = input("Deseja modificar o check [s/n]: ")
+
+                        
+                            if mod == "s":
+                                newcheck = input("Novo Check [True|False]: ").strip()
+                                arq[i] = newcheck
+                    
+                with open("rota.txt", "w") as rota:
+                    rota.write("\n".join(arq))
+
+                print("\nRota atualizada com sucesso")
+
+                opc = int(input("\nDigite [1] para voltar:  "))
+                sleep(0.5)
+                screen.clear()
+
+            elif opc == 3:
+                while True:
+                    screen.clear()
+                    print('''
+            [1]: Criar Aviso
+            [2]: Visualizar Avisos
+            [3]: Voltar''')
+                    opc = int(input("opc = "))
+                    if opc == 1:
+                        GeralDef.CreateNotice()
+                    elif opc == 2:
+                        avisos = open("avisos.txt", "r", encoding="utf-8")
+                        linhasavisos = avisos.read().splitlines()
+                        avisos.close()
+                        screen.clear()
+                        for aviso in linhasavisos:
+                            print(aviso)
+                        voltar = str(input("\nDigite [1] para voltar: "))
+                    elif opc == 3:
+                        break
+                    else:
+                        screen.clear()
+                        print("Selecione uma opção valida...")
+                        sleep(2)
+
+            elif opc == 4:
+                screen.clear()
+                break
+
+            else:
+                print("Opção inválida :(")
+                print("Tente novamente")
