24a32eb02d05a36cf15172028fa8f9cfa4d7e948
ix5025887-lgtm <ix5025887@gmail.com>
2025-11-12 15:43:04 -0300

    novas funções screen

--- functions/screen/__init__.py
+++ functions/screen/__init__.py
@@ -15,9 +15,11 @@ def menuADM():
 [1]: Lista de Alunos
 [2]: Editar Universidades
 [3]: Editar Alunos
-[4]: Acompanhar Rota
-[5]: Adicionar Aviso
-[6]: Sair''')
+[4]: Editar motoristas
+[5]: Acompanhar Rota
+[6]: Adicionar Aviso
+[7]: Sua conta
+[0]: Sair''')
     
 def menuUniversidades():
     print('''
@@ -27,3 +29,28 @@ def menuUniversidades():
 [3]: Editar Universidade
 [4]: Excluir Universidade
 [0]: Voltar''')
+
+def menuADM_sua_conta():
+    print('''-------------------------
+        Sua conta  
+-------------------------
+          
+[1]: Informações pessoais
+[2]: Credênciais
+[0]: Voltar
+          ''')
+    
+    opc=int(input("Digite: "))
+    return opc
+
+def editar_usuário_menu(tipo_de_usuário):
+    print(f"""-------------------------
+    Editar {tipo_de_usuário}  
+-------------------------          
+[1]: adicionar {tipo_de_usuário}
+[2]: remover {tipo_de_usuário}
+[3]: editar {tipo_de_usuário}
+[0]: Voltar
+""")
+    opc=int(input("Por favor selecione uma opção: "))
+    return opc
