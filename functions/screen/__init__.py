55834be32a6cad017bb2ca896d98b93e8d65d006
dudu <ghiashitaka@gmail.com>
2025-12-02 23:01:35 -0300

    main com arquivos txt atualizados (como por exemplo o das rotas) só falta integrar esse arquivo novo com o resto do cód como o do adm, o do aluno já lê ele normal

--- functions/screen/__init__.py
+++ functions/screen/__init__.py
@@ -4,52 +4,17 @@ def clear():
 
 def menu():
     print('''
-==Bem vindo ao Unirota!==
+Bem vindo ao Unirota!
+
 [1]: Fazer Login
 [2]: Redefinir Senha
 [3]: Sair''')
 
 def menuADM():
     print('''
-==== GERENCIAR UNIVERSIDADES ====         
 [1]: Lista de Alunos
 [2]: Editar Universidades
 [3]: Editar Alunos
-[4]: Editar motoristas
-[5]: Acompanhar Rota
-[6]: Adicionar Aviso
-[7]: Sua conta
-[0]: Sair''')
-    
-def menuUniversidades():
-    print('''
-==== GERENCIAR UNIVERSIDADES ====
-[1]: Cadastrar Universidade
-[2]: Listar Universidades
-[3]: Editar Universidade
-[4]: Excluir Universidade
-[0]: Voltar''')
-
-def menuADM_sua_conta():
-    print('''
-======= Sua conta =======    
-[1]: Informações pessoais
-[2]: Credênciais
-[0]: Voltar
-          ''')
-    
-    opc=int(input("Digite: "))
-    return opc
-
-def editar_usuário_menu(tipo_de_usuário):
-    print(f"""
-==== Editar {tipo_de_usuário} ====    
-[1]: adicionar {tipo_de_usuário}
-[2]: remover {tipo_de_usuário}
-[3]: editar {tipo_de_usuário}
-[0]: Voltar
-""")
-    opc=int(input("Por favor selecione uma opção: "))
-    return opc
-    
-    
+[4]: Acompanhar Rota
+[5]: Adicionar Aviso
+[6]: Sair''')
